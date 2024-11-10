4f76aa3f01a5e4c741b7f3af0380988e30c413f5
maichu <chuthimai18245@gmail.com>
2024-11-10 23:19:38 +0700

    sửa lại link theo IP

--- main.py
+++ main.py
@@ -88,8 +88,8 @@ def get_store_state():
 
 if __name__ == "__main__":
     app.run(
-        debug=True,
-        # host='0.0.0.0',
+        debug=False,
+        host='0.0.0.0',
         port=5000
     )
 
